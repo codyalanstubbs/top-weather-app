8f8918cd83a2a5b7323c2f2fef206f813048f813
codyastubbs <5488363+codyastubbs@users.noreply.github.com>
2022-11-03 16:07:00 -0400

    Add searching feature for user to find weather for their city

--- script.js
+++ script.js
@@ -2,11 +2,17 @@ const weatherIcon = document.querySelector(".icon");
 const locationDiv = document.querySelector(".location");
 const conditions = document.querySelector(".conditions");
 const feels = document.querySelector(".feels");
+const humidity = document.querySelector(".humid");
 const lo = document.querySelector(".lo");
 const hi = document.querySelector(".hi");
 const windSpeed = document.querySelector(".windSpeed");
+const windDirection = document.querySelector(".windDirection");
 const locationSearch = document.querySelector("#search");
 const submitBtn = document.querySelector("#submit");
+
+submitBtn.addEventListener("click", (e) => {
+    getData(locationSearch.value);
+})
 
 getData("Harrisburg, Pennslyvania");
 
@@ -21,7 +27,7 @@ async function getData(locationQuery) {
         const data = processData(json);
         displayData(data);
     } catch(error) {
-        console.log("Error: ", error);
+        alert("Whoops! Try again!");
     }
 }
 
@@ -30,12 +36,12 @@ function processData(json) {
         location: json.name,
         description: json.weather[0].description,
         icon: json.weather[0].icon,
-        temp: json.main.temp,
-        hi: json.main.temp_max,
-        lo: json.main.temp_min,
-        feels: json.main.feels_like,
+        temp: json.main.temp.toFixed(1),
+        hi: json.main.temp_max.toFixed(1),
+        lo: json.main.temp_min.toFixed(1),
+        feels: json.main.feels_like.toFixed(1),
         humid: json.main.humidity,
-        wSpeed: json.wind.speed,
+        wSpeed: json.wind.speed.toFixed(1),
         wDirection: json.wind.deg
     };
 }
@@ -45,10 +51,11 @@ function displayData(data) {
     locationDiv.textContent = data.location;
     conditions.textContent = capitalizeDescription(data.description) + " & " + data.temp + "°F";;
     feels.textContent = "Feels like: " + data.feels + "°F";
+    humidity.textContent = "Humidity: " + data.humid + "%";
     lo.textContent = "LO: " + data.lo + "°F";
     hi.textContent = "HI: " + data.hi + "°F";
     windSpeed.textContent = "Wind Speed: " + data.wSpeed + " MPH";
-    windDirection.textContent = "Wind Direction: " + data.wDirection;
+    windDirection.textContent = "Wind Direction: " + data.wDirection + "°";
 }
 
 function capitalizeDescription(description) {
