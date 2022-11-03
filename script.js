bdf77166e1a47521566de555f5b748a2bfc11655
codyastubbs <5488363+codyastubbs@users.noreply.github.com>
2022-11-03 15:48:30 -0400

    Add functions to process and display data

--- script.js
+++ script.js
@@ -1,14 +1,58 @@
-async function getData(location) {
+const weatherIcon = document.querySelector(".icon");
+const locationDiv = document.querySelector(".location");
+const conditions = document.querySelector(".conditions");
+const feels = document.querySelector(".feels");
+const lo = document.querySelector(".lo");
+const hi = document.querySelector(".hi");
+const windSpeed = document.querySelector(".windSpeed");
+const locationSearch = document.querySelector("#search");
+const submitBtn = document.querySelector("#submit");
+
+getData("Harrisburg, Pennslyvania");
+
+async function getData(locationQuery) {
     try {
-        location = location.replace(/ /g,'');
-        const url = "https://api.openweathermap.org/data/2.5/weather?q="+location+"&APPID=f7f9c4ef474adaa8f7584523bd3abfb9";
-        console.log(url);
+        locationQuery = locationQuery.replace(/ /g,'');
+        const url = "https://api.openweathermap.org/data/2.5/weather?q="+locationQuery+"&units=imperial&APPID=f7f9c4ef474adaa8f7584523bd3abfb9";
+        
         const response = await fetch(url, {mode: 'cors'});
         const json = await response.json();
-        console.log(json);
+
+        const data = processData(json);
+        displayData(data);
     } catch(error) {
         console.log("Error: ", error);
     }
 }
 
-getData("Harrisburg, Pennslyvania");
+function processData(json) {
+    return {
+        location: json.name,
+        description: json.weather[0].description,
+        icon: json.weather[0].icon,
+        temp: json.main.temp,
+        hi: json.main.temp_max,
+        lo: json.main.temp_min,
+        feels: json.main.feels_like,
+        humid: json.main.humidity,
+        wSpeed: json.wind.speed,
+        wDirection: json.wind.deg
+    };
+}
+
+function displayData(data) {
+    weatherIcon.src = "http://openweathermap.org/img/wn/"+data.icon+"@2x.png";
+    locationDiv.textContent = data.location;
+    conditions.textContent = capitalizeDescription(data.description) + " & " + data.temp + "°F";;
+    feels.textContent = "Feels like: " + data.feels + "°F";
+    lo.textContent = "LO: " + data.lo + "°F";
+    hi.textContent = "HI: " + data.hi + "°F";
+    windSpeed.textContent = "Wind Speed: " + data.wSpeed + " MPH";
+    windDirection.textContent = "Wind Direction: " + data.wDirection;
+}
+
+function capitalizeDescription(description) {
+    const firstLetterCapitalized = description.charAt(0).toUpperCase();
+    const remainingLetters = description.slice(1);
+    return firstLetterCapitalized + remainingLetters;
+}
